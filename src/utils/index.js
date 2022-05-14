f388c86c5ed658ac126aec9ae62294440320c671
chengs <nishigi@163.com>
2022-05-14 23:58:53 +0800

    fix: delete <React.StrictMode>

--- src/utils/index.js
+++ src/utils/index.js
@@ -19,3 +19,29 @@ export const useMount = callback => {
         callback();
     }, []); // eslint-disable-line
 };
+
+export const _debounce = (func, delay) => {
+    let timer = null;
+    return () => {
+        if (timer) {
+            clearTimeout(timer);
+        }
+        timer = setTimeout(() => {
+            func();
+        }, delay);
+    };
+};
+export const _throttle = (func, wait) => {
+    let timer = null;
+    let flag = false;
+    return () => {
+        if (flag) {
+            return;
+        }
+        flag = true;
+        timer = setTimeout(() => {
+            flag = false;
+            func();
+        }, wait);
+    };
+};
