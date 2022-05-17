2c62ebe177d0460b3a1e00651bbc8fdfc16c7b61
chengs <nishigi@163.com>
2022-05-17 16:54:06 +0800

    feat: add login&middleWare

---
+++ __json_server_mock__/middleWare.js
@@ -0,0 +1,12 @@
+module.exports = (req, res, next) => {
+    if (req.method === 'POST' && req.path === '/login') {
+        if (req.body.username === 'zxc' && req.body.password === '123') {
+            return res.status(200).json({
+                user: { token: 'aha' },
+            });
+        } else {
+            return res.status(400).json({ message: '用户信息不正确' });
+        }
+    }
+    next();
+};
--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import './App.css';
-import { ProjectListScreen } from 'screens/project-list';
-import { TsReactTest } from "screens/try-use-array";
+// import { ProjectListScreen } from 'screens/project-list';
+import { LoginScreens } from 'screens/login';
+// import { TsReactTest } from "screens/try-use-array";
+
 
 function App() {
   return (
     <div className="App">
-      <ProjectListScreen></ProjectListScreen>
-      <TsReactTest></TsReactTest>
+      {/* <ProjectListScreen></ProjectListScreen>
+      <TsReactTest></TsReactTest> */}
+      <LoginScreens></LoginScreens>
     </div>
   );
 }
